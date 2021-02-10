3b2741c3d20b03c6b50412de784c40ca8a0c2f70
alhas <bahtiyaralialhas@gmail.com>
2021-02-10 19:16:07 +0100

    CI/CD Configuration and Docker push.

--- test/test_views.py
+++ test/test_views.py
@@ -1,2 +1,40 @@
+import unittest
+
+from selenium import webdriver
+
+PATH = "/usr/bin/chromedriver"
 
 
+class TestViews(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(executable_path=PATH)
+        cls.driver.get("http://0.0.0.0:80/")
+
+    def test_buttons(self):
+        self.driver.find_element_by_name("one").click()
+        self.driver.find_element_by_name("two").click()
+        self.driver.find_element_by_name("three").click()
+        self.driver.find_element_by_name("times").click()
+        self.driver.find_element_by_name("three").click()
+        self.driver.find_element_by_id("result").click()
+        actualvalue = self.driver.find_element_by_id("display").get_attribute("value")
+        assert actualvalue == "369"
+
+    def test_inputs(self):
+        self.driver.find_element_by_id("clear").click()
+        self.driver.find_element_by_name("display").send_keys(1234 * 123)
+        self.driver.find_element_by_id("result").click()
+        actualvalue = self.driver.find_element_by_id("display").get_attribute("value")
+        assert actualvalue == "151782"
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+        print("Test Completed")
+
+
+if __name__ == "__main__":
+    unittest.main()
