0aab8be3732d9b845247a6ace917c2b68b927db7
alhas <bahtiyaralialhas@gmail.com>
2021-02-10 19:22:30 +0100

    Travis CI/CD Configuration and Docker push.

---
+++ .travis/deploy_dockerhub.sh
@@ -0,0 +1,16 @@
+FROM python:3.8.3-alpine
+MAINTAINER alhas bahtiyaralialhas@gmail.com
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
+COPY service/ ./service
+COPY app.py .
+
+CMD ["python","./app.py"]
+
+
+
